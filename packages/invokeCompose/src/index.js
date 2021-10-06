1d0eb28b1f8dbb04eeee6b7065951f11331c14a3
Gary <bugofbook@gmail.com>
2021-10-06 13:54:52 +0800

    test2

--- packages/invokeCompose/src/index.js
+++ packages/invokeCompose/src/index.js
@@ -1,5 +1,5 @@
 // export { default as compose } from './common';
-// export * from './common';
+// export * from './common'111;
 // export { default as CircularProgress } from './CircularProgress';
 const invokeCompose = require('./common')
 module.export = invokeCompose
